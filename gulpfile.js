18489bb70f65a5e88e2ef816cc4683b09afd3ca5
Andrew Usher <andrewusher00@gmail.com>
2018-01-29 10:38:27 -0600

    Inital commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const htmlMin = require('gulp-htmlmin');
+const imageMin = require('gulp-imagemin');
+const stylus = require('gulp-stylus');
+
+gulp.task('img', function () {
+  return gulp.src('src/img/*')
+    .pipe(imageMin())
+    .pipe(gulp.dest('docs/img'))
+});
+
+gulp.task('html', function () {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlMin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('docs'));
+});
+
+gulp.task('js', function () {
+  return gulp.src('src/js/*.js')
+    .pipe(babel({
+      comments: false,
+      minified: true
+    }))
+    .pipe(gulp.dest('docs/js'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('src/styl/style.styl')
+    .pipe(stylus({
+      compress: true,
+    }))
+    .pipe(gulp.dest('docs/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/styl/*.styl', ['css'])
+  gulp.watch('src/*.html', ['html'])
+  gulp.watch('src/js/*.js', ['js'])
+});
