ce64713b6ef3ea38a663145436a1c90d7a303e1e
Andrew Usher <andrewusher00@gmail.com>
2019-05-21 14:18:13 -0500

    Switch to yarn and update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,48 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const htmlMin = require('gulp-htmlmin');
-const imageMin = require('gulp-imagemin');
-const stylus = require('gulp-stylus');
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const htmlMin = require('gulp-htmlmin')
+const imageMin = require('gulp-imagemin')
+const stylus = require('gulp-stylus')
 
-gulp.task('img', function () {
-  return gulp.src('src/img/*')
+gulp.task('img', function() {
+  return gulp
+    .src('src/img/*')
     .pipe(imageMin())
     .pipe(gulp.dest('docs/img'))
-});
+})
 
-gulp.task('html', function () {
-  return gulp.src('src/**/*.html')
+gulp.task('html', function() {
+  return gulp
+    .src('src/**/*.html')
     .pipe(htmlMin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('docs'));
-});
+    .pipe(gulp.dest('docs'))
+})
 
-gulp.task('js', function () {
-  return gulp.src('src/js/*.js')
-    .pipe(babel({
-      comments: false,
-      minified: true
-    }))
-    .pipe(gulp.dest('docs/js'));
-});
+gulp.task('js', function() {
+  return gulp
+    .src('src/js/*.js')
+    .pipe(
+      babel({
+        comments: false,
+        minified: true
+      })
+    )
+    .pipe(gulp.dest('docs/js'))
+})
 
-gulp.task('css', function () {
-  return gulp.src('src/styl/style.styl')
-    .pipe(stylus({
-      compress: true,
-    }))
-    .pipe(gulp.dest('docs/css'));
-});
+gulp.task('css', function() {
+  return gulp
+    .src('src/styl/style.styl')
+    .pipe(
+      stylus({
+        compress: true
+      })
+    )
+    .pipe(gulp.dest('docs/css'))
+})
 
-gulp.task('watch', function () {
-  gulp.watch('src/styl/*.styl', ['css'])
-  gulp.watch('src/*.html', ['html'])
-  gulp.watch('src/js/*.js', ['js'])
-});
+gulp.task('watch', function() {
+  gulp.watch('src/styl/*.styl', gulp.series('css'))
+  gulp.watch('src/*.html', gulp.series('html'))
+  gulp.watch('src/js/*.js', gulp.series('js'))
+})
